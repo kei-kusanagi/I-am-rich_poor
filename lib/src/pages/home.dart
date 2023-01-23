eb96ee4a9ec54dc51fd0195292cf5764428f6ff7
kei-kusanagi <kei_kusanagi@outlook.com>
2023-01-23 11:18:33 -0600

    add buttonbar and fix snackbar

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -7,24 +7,39 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String state='';
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-          title: Text('I am $state'),
-          actions: <Widget>[
-      IconButton(
-      icon: const Icon(Icons.add_card),
-      tooltip: 'Show Snackbar',
-      onPressed: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Hola soy un snackbar')));
-      },
-    ),
-          ],
-    ),
 
-      // Text('hola'),
+    return Scaffold(
+      // backgroundColor: Colors.redAccent,
+      bottomNavigationBar:
+      BottomAppBar(
+        color: Colors.black,
+        child: Container(
+
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+
+
+
+          children: <Widget>[
+
+
+            IconButton(
+
+              icon: const Icon(Icons.add_card),
+              tooltip: 'Show Snackbar',
+
+              color: Colors.amberAccent,
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Hola soy un snackbar')));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+
+
       body: LiquidSwipe(
         pages: liquidPages,
         fullTransitionValue: 300,
--- lib/src/pages/liquid_pages.dart
+++ lib/src/pages/liquid_pages.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
+String estado='rich';
+
 
 final liquidPages = [
-  Container(
+  Scaffold(
+    appBar: AppBar(
+title: Text('I am poor'),
+),
+  body: Container(
     color: Colors.deepPurple,
-    child: const Padding(
-      padding: EdgeInsets.all(8),
-      child: SafeArea(
-        child: Center(
-                child: Image(
-                image: AssetImage('images/calamardo.gif'),
-                ),
+      child: const Padding(
+        padding: EdgeInsets.all(8),
+        child: SafeArea(
+          child: Center(
+                  child: Image(
+                  image: AssetImage('images/calamardo.gif'),
+                  ),
+          ),
         ),
       ),
     ),
   ),
-  Container(
+
+  Scaffold(
+appBar: AppBar(
+  title: Text('I am rich'),
+  //   actions: <Widget>[
+  //   IconButton(
+  //   icon: const Icon(Icons.add_card),
+  //   tooltip: 'Show Snackbar',
+  //   onPressed: () {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(content: Text('Hola soy un snackbar')));
+  //   },
+  // ),
+  //       ],
+),
+  body: Container(
     color: Colors.pink,
     child: const Padding(
       padding: EdgeInsets.all(8),
@@ -23,9 +45,24 @@ final liquidPages = [
 
             child: Image(
               image: AssetImage('images/diamond.png'),
+
             ),
         ),
       ),
     ),
   ),
+  ),
 ];
+
+// Future<String> returnString() async {
+//   return "rich";
+// }
+
+// String returnString(String input) {
+//   if (input == "hello") {
+//     return "world";
+//   } else {
+//     return "I don't understand what you mean by '$input'";
+//   }
+// }
+//
